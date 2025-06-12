0932284a2dec5e36e9b8228f8b99d5a444cd52ba
HenryKaum <hccm503@gmail.com>
2025-06-12 19:29:06 -0300

    Adicao da classe consulta clinica

--- MariaAlvezApp/admin.py
+++ MariaAlvezApp/admin.py
@@ -1,18 +1,61 @@
+# MariaAlvezApp/admin.py
 from django.contrib import admin
 from .models import (
-    Veterinario, Tutor, Animal, ConsultaClinica,
-    AgendamentoConsultas, RegistroVacinacao,
-    RegistroVermifugos, Exames, Medicamentos
+    Veterinario,
+    Tutor,
+    Animal,
+    ConsultaClinica,
+    AgendamentoConsultas,
+    RegistroVacinacao,
+    RegistroVermifugos,
+    Exames,
+    Medicamentos
 )
 
-# Register your models here.
-admin.site.register(Veterinario)
-admin.site.register(Tutor)
-admin.site.register(Animal)
-admin.site.register(ConsultaClinica)
-admin.site.register(AgendamentoConsultas)
+#Veterinario (essenciais para ConsultaClinica)
+@admin.register(Veterinario)
+class VeterinarioAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+#Tutor (essenciais para ConsultaClinica)
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+#Animal
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'especie', 'tutor')
+    search_fields = ('nome', 'especie', 'rfid')
+    autocomplete_fields = ('tutor',)
+
+#Exames (essenciais para ConsultaClinica)
+@admin.register(Exames)
+class ExamesAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+#Medicamentos (essenciais para ConsultaClinica)
+@admin.register(Medicamentos)
+class MedicamentosAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+
+# ConsultaClinica
+@admin.register(ConsultaClinica)
+class ConsultaClinicaAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('vet_responsavel', 'animal', 'exames')
+    list_display = ('animal', 'data_atendimento', 'vet_responsavel', 'tipo_atendimento')
+    search_fields = ('animal__nome', 'vet_responsavel__nome', 'diagnostico')
+
+#AgendamentoConsultas (essenciais para ConsultaClinica)
+@admin.register(AgendamentoConsultas)
+class AgendamentoConsultasAdmin(admin.ModelAdmin):
+    list_display = ('data_consulta', 'tutor', 'animal')
+    search_fields = ('data_consulta', 'tutor__nome', 'animal__nome')
+
 admin.site.register(RegistroVacinacao)
 admin.site.register(RegistroVermifugos)
-admin.site.register(Exames)
-admin.site.register(Medicamentos)
-
