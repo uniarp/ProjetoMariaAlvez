76e9e9f374e0f2fa94073548f7f40f37fa39a51f
Vitor <vitorgabrielb47@gmail.com>
2025-06-12 21:38:06 -0300

    Adicionado classes Registro de vacinação e registro de Vermifugos

--- MariaAlvezApp/models.py
+++ MariaAlvezApp/models.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import timedelta
 from django.db import models
+
 
 # Classes principais
 class Veterinario(models.Model):
@@ -28,14 +32,50 @@ class AgendamentoConsultas(models.Model):
 
 # Classes de procedimentos"
 class RegistroVacinacao(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, blank=True, null=True)
+    medicamento = models.ForeignKey('Medicamentos', on_delete=models.CASCADE,  blank=True, null=True)
+    data_aplicacao = models.DateField( blank=True, null=True)
+    data_revacinacao = models.DateField(verbose_name="Data Revacinação", blank=True, null=True, )
+
     class Meta:
-        verbose_name = "Registro de Vacinacão"
-        verbose_name_plural = "Registros de Vacinações"
+        verbose_name = "Registro de Vacinação"
+        verbose_name_plural = "Registros de Vacinação"
+
+    def clean(self):
+        hoje = timezone.now().date()
+        limite = hoje - timedelta(days=15)
+
+        if self.data_aplicacao < limite:
+            raise ValidationError({'data_aplicacao': 'A data de aplicação não pode ser anterior a 15 dias.'})
+
+        if self.data_revacinacao and self.data_revacinacao < limite:
+            raise ValidationError({'data_revacinacao': 'A data de revacinação não pode ser anterior a 15 dias.'})
+
+    def __str__(self):
+        return f" {self.data_aplicacao}"
 
 class RegistroVermifugos(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE, blank=True, null=True)
+    medicamento = models.ForeignKey('Medicamentos', on_delete=models.CASCADE,  blank=True, null=True)
+    data_administracao = models.DateField( blank=True, null=True)
+    data_readministracao = models.DateField(verbose_name="Data Readministração", blank=True, null=True, )
+
     class Meta:
         verbose_name = "Registro de Vermífugo"
         verbose_name_plural = "Registros de Vermífugos"
+
+    def clean(self):
+        hoje = timezone.now().date()
+        limite = hoje - timedelta(days=15)
+
+        if self.data_administracao < limite:
+            raise ValidationError({'data_administracao': 'A data de administração não pode ser anterior a 15 dias.'})
+
+        if self.data_readministracao and self.data_readministracao < limite:
+            raise ValidationError({'data_readministracao': 'A data de readministração não pode ser anterior a 15 dias.'})
+
+    def __str__(self):
+        return f" {self.data_administracao}"
 
 class Exames(models.Model):
     class Meta:
