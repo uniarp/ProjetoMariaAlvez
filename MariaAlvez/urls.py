3adf38465cc70d8277ff9fbfe2dab18685ab1c51
beto <humbertopohlenz@gmail.com>
2025-06-13 08:24:09 -0300

    Implementa View de Relatórios e templates relacionados

--- MariaAlvez/urls.py
+++ MariaAlvez/urls.py
@@ -1,6 +1,14 @@
+from MariaAlvezApp import views
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('relatorio/consultas/', views.gerar_relatorio_consultas, name='relatorio_consultas'),
+    path('relatorio/estoque/', views.relatorio_estoque, name='relatorio_estoque'),
+    path('fila/castracao/', views.fila_castracao, name='fila_castracao'),
+    path('relatorio/vacinacao/', views.relatorio_vacinacao, name='relatorio_vacinacao'),
+    path('relatorio/vermifugo/', views.relatorio_vermifugo, name='relatorio_vermifugo'),
+    
 ]
